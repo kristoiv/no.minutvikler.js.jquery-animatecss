0e2a9c4ed9fed9fce52f7ddcfaa55ebe6d3c7783
Kristoffer A. Iversen <kristoffer.a.iversen@gmail.com>
2014-09-09 00:41:23 +0200

    Changed the name of the parameter from wait to delay

--- jquery-animatecss.js
+++ jquery-animatecss.js
@@ -12,25 +12,25 @@
      *              interface and an optional sleep thereafter before the
      *              callback is initiated.
      */
-    $.fn.animateCss = function(name, callback, wait) {
-        var func = function(elm, n, cb, w) {
-            if (w === undefined || w === null) {
-                w = 0;
+    $.fn.animateCss = function(name, callback, delay) {
+        var func = function(elm, n, cb, d) {
+            if (d === undefined || d === null) {
+                d = 0;
             }
             elm.addClass('animated ' + n);
             elm.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
                 elm.removeClass('animated ' + n);
                 if (typeof cb === 'function') {
-                    setTimeout(cb, w);
+                    setTimeout(cb, d);
                 }
             });
         }
         if (this.length > 1) {
             this.each(function() {
-                func($(this), name, callback, wait);
+                func($(this), name, callback, delay);
             });
         }else{
-            func(this, name, callback, wait);
+            func(this, name, callback, delay);
         }
         return true;
     }
