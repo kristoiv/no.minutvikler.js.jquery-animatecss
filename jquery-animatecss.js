cf24b6df2f8d1c920614149e7d5c7c2cadbd0a65
Kristoffer A. Iversen <kristoffer.a.iversen@gmail.com>
2014-09-08 23:27:09 +0200

    Initial commit, animate css jquery plugin

---
+++ jquery-animatecss.js
@@ -0,0 +1,33 @@
+
+(function($) {
+    /**
+     * Copyright 2014, Min Utvikler
+     * http://minutvikler.no/
+     *
+     * Licensed under the MIT license.
+     *
+     * @author Kristoffer A. Iversen
+     * @description A small plugin that simplifies adding animations to
+     *              jQuery selections using animate.css. Provides a callback
+     *              interface and an optional sleep thereafter before the
+     *              callback is initiated.
+     */
+    $.fn.animateCss = function(name, callback, wait) {
+        var func = function(elm, n, cb, w) {
+            elm.addClass('animated ' + n);
+            elm.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+                elm.removeClass('animated ' + n);
+                setTimeout(cb, w);
+            });
+        }
+        if (this.length > 1) {
+            this.each(function() {
+                func($(this), name, callback, wait);
+            });
+        }else{
+            func(this, name, callback, wait);
+        }
+        return true;
+    }
+})(jQuery);
+
